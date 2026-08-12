22974c5893c7d60e98761556b54c055c7891424d
Ahmed-Thabet000 <ahmedthabet202006@gmail.com.com>
2026-08-12 23:40:50 +0300

    Home

--- shared/nav.php
+++ shared/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar fixed-top navbar-expand-lg  mb-5 border border-bottom-primary" data-bs-theme="dark" style="background-color:  rgb(8, 8, 87);">
     <div class="container-fluid">
-        <a class="navbar-brand fw-bold px-3" href="/Summer_training_2026/Projects/index.php">ANALGIA</a>
+        <a class="navbar-brand fw-bold px-3" href="/Summer_training_2026/NTI_final_project/NTI_2026_final_project_DashBoard/index.php">ANALGIA</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -11,8 +11,8 @@
                         Categories
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="/Summer_training_2026/Projects/Categories/add.php">Add Category</a></li>
-                        <li><a class="dropdown-item" href="/Summer_training_2026/Projects/Categories/list.php">List Categories</a></li>
+                        <li><a class="dropdown-item" href="">Add Category</a></li>
+                        <li><a class="dropdown-item" href="">List Categories</a></li>
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
@@ -20,8 +20,8 @@
                         Products
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="/Summer_training_2026/Projects/Products/add.php">Add Product</a></li>
-                        <li><a class="dropdown-item" href="/Summer_training_2026/Projects/Products/list.php">List Products</a></li>
+                        <li><a class="dropdown-item" href="">Add Product</a></li>
+                        <li><a class="dropdown-item" href="">List Products</a></li>
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
@@ -29,8 +29,8 @@
                         Clients
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="/Summer_training_2026/Projects/Clients/add.php">Add Client</a></li>
-                        <li><a class="dropdown-item" href="/Summer_training_2026/Projects/Clients/list.php">List Clients</a></li>
+                        <li><a class="dropdown-item" href="">Add Client</a></li>
+                        <li><a class="dropdown-item" href="">List Clients</a></li>
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
@@ -38,8 +38,8 @@
                         Brands
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="/Summer_training_2026/Projects/Clients/add.php">Add Brands</a></li>
-                        <li><a class="dropdown-item" href="/Summer_training_2026/Projects/Clients/list.php">List Brands</a></li>
+                        <li><a class="dropdown-item" href="">Add Brands</a></li>
+                        <li><a class="dropdown-item" href="">List Brands</a></li>
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
@@ -47,8 +47,8 @@
                         Employees
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="/Summer_training_2026/Projects/Clients/add.php">Add Employees</a></li>
-                        <li><a class="dropdown-item" href="/Summer_training_2026/Projects/Clients/list.php">List Employees</a></li>
+                        <li><a class="dropdown-item" href="">Add Employees</a></li>
+                        <li><a class="dropdown-item" href="">List Employees</a></li>
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
@@ -56,8 +56,8 @@
                         Partners
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="/Summer_training_2026/Projects/Clients/add.php">Add Partners</a></li>
-                        <li><a class="dropdown-item" href="/Summer_training_2026/Projects/Clients/list.php">List Partners</a></li>
+                        <li><a class="dropdown-item" href="">Add Partners</a></li>
+                        <li><a class="dropdown-item" href="">List Partners</a></li>
                     </ul>
                 </li>
             </ul>
